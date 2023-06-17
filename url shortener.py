8b859ea6f92333cf49711a390d34c2c7158a649a
bizzart123 <91026392+bizzart123@users.noreply.github.com>
2023-06-17 21:15:31 +0530

    Add files via upload

---
+++ url shortener.py
@@ -0,0 +1,11 @@
+
+import pyshorteners
+url=input("Enter the URL:")
+
+def shortenurl(url):
+    s = pyshorteners.Shortener()
+    print(s.tinyurl.short(url))
+print("URL Shortenered form:")
+shortenurl(url)
+
+
